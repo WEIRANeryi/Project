051bf51bca51cbcef8f2c4304814ad7ebcbb89f1
WEIRANeryi <741758994@qq.com>
2019-04-13 19:35:15 +0800

    修改

--- cloud-book/src/utils/api.js
+++ cloud-book/src/utils/api.js
@@ -1,13 +1,13 @@
 export default {
   getSwiper: '/swiper',//获取轮播图数据
-  getContent: '/category/books', //根据分类来获取图书数据
-  getBook: '/book',//获取单个图书数据
-  getTitle: '/titles',//获取图书目录
-  getArticle: '/article', //获取单个文章内容
-  postRegister: '/register',//发送注册请求
-  sendCode: '/phoneValidator',//发送手机验证码
-  postLogin: '/login_html',//h5专用登录接口
-  getUser: '/user',//获取用户信息
+  getContent: '/category/books', //根据分类来获取图书数据 //已完成
+  getBook: '/book',//获取单个图书数据                     //已完成
+  getTitle: '/titles',//获取图书目录                      //已完成
+  getArticle: '/article', //获取单个文章内容              //已完成
+  postRegister: '/register',//发送注册请求                //已完成
+  sendCode: '/phoneValidator',//发送手机验证码            //已完成
+  postLogin: '/login_html',//h5专用登录接口               //已完成
+  getUser: '/user',//获取用户信息                         //已完成
   getRead: '/readList',//获取阅读过的图书
   addCollection: '/collection',//添加收藏
   changeUser: '/user',//修改用户信息
